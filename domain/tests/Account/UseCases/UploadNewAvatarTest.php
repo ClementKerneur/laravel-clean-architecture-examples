cb71e18f950acb69651598e8f9e3847b825458f3
Alexandre Gérault <alexandre@hexium.io>
2021-07-16 15:07:09 +0200

    tests(account): test that user cannot upload a new avatar

---
+++ domain/tests/Account/UseCases/UploadNewAvatarTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Domain\Test\Account\UseCases;
+
+use Domain\Account\UseCases\UploadNewAvatar\UploadNewAvatar;
+use Domain\Account\UseCases\UploadNewAvatar\UploadNewAvatarPresenterInterface;
+use Domain\Account\UseCases\UploadNewAvatar\UploadNewAvatarRequest;
+use Domain\Account\UseCases\UploadNewAvatar\UploadNewAvatarResponse;
+use Domain\Test\Account\Adapters\AvatarsStorageAdapter;
+use PHPUnit\Framework\TestCase;
+
+class UploadNewAvatarTest extends TestCase implements UploadNewAvatarPresenterInterface
+{
+    public UploadNewAvatarResponse $response;
+    
+    public function presents(UploadNewAvatarResponse $response): void
+    {
+        $this->response = $response;
+    }
+
+    public function test_the_response_has_an_error_if_user_already_has_at_least_4_avatar()
+    {
+        $storage = new AvatarsStorageAdapter();
+        $useCase = new UploadNewAvatar($storage);
+        $request = new UploadNewAvatarRequest();
+
+        $request->username = "has_5_avatars";
+
+        $useCase->execute($request, $this);
+
+        $this->assertNotNull($this->response->error);
+    }
+}
