5c4fa3518fa21d2f17d53a6e595f7de7f092e97f
Alexandre Gérault <alexandre@hexium.io>
2021-07-16 15:17:25 +0200

    feat(account): define a method to store a new avatar

--- domain/src/Account/Ports/AvatarsStoragePort.php
+++ domain/src/Account/Ports/AvatarsStoragePort.php
@@ -5,4 +5,6 @@ namespace Domain\Account\Ports;
 interface AvatarsStoragePort
 {
     public function getAmountOfAvatarsForUsername(string $username): int;
+
+    public function uploadAndAddNewAvatarToUser(string $avatarContent, string $username): void;
 }
